ca929b537017e26e4d98b4fe36b18d73692ced13
Nathan <121035981+NaTRueg@users.noreply.github.com>
2023-02-08 15:33:37 +0100

    Add files via upload

---
+++ newsletter.sql
@@ -0,0 +1,176 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Feb 08, 2023 at 02:32 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `newsletter`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `interest`
+--
+
+CREATE TABLE `interest` (
+  `id` int NOT NULL,
+  `interest_label` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `interest`
+--
+
+INSERT INTO `interest` (`id`, `interest_label`) VALUES
+(1, 'Peinture'),
+(2, 'Sculpture'),
+(3, 'Photographie'),
+(4, 'Art contemporain'),
+(5, 'Films'),
+(6, 'Art numérique'),
+(7, 'Installations');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `origin`
+--
+
+CREATE TABLE `origin` (
+  `id` int NOT NULL,
+  `origine_label` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `origin`
+--
+
+INSERT INTO `origin` (`id`, `origine_label`) VALUES
+(1, 'Un ami m’en a parlé'),
+(2, 'Recherche sur internet'),
+(3, 'Publicité dans un magazine');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscribers`
+--
+
+CREATE TABLE `subscribers` (
+  `id` int NOT NULL,
+  `createThe` date DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `origine_id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `subscribers`
+--
+
+INSERT INTO `subscribers` (`id`, `createThe`, `email`, `firstName`, `lastName`, `origine_id`) VALUES
+(1, NULL, 'alfred.dupont@gmail.com', 'Alfred', 'Dupont', NULL),
+(2, NULL, 'b.lav@hotmail.fr', 'Bertrand', 'Lavoisier', NULL),
+(3, NULL, 'SarahLAMINE@gmail.com', 'Sarah', 'Lamine', NULL),
+(4, NULL, 'mo78@laposte.net', 'Mohamed', 'Ben Salam', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscriber_interest`
+--
+
+CREATE TABLE `subscriber_interest` (
+  `subscribers_id` int NOT NULL,
+  `interest_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `interest`
+--
+ALTER TABLE `interest`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `origin`
+--
+ALTER TABLE `origin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subscribers`
+--
+ALTER TABLE `subscribers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `key` (`origine_id`);
+
+--
+-- Indexes for table `subscriber_interest`
+--
+ALTER TABLE `subscriber_interest`
+  ADD PRIMARY KEY (`subscribers_id`,`interest_id`),
+  ADD KEY `interest_id` (`interest_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `interest`
+--
+ALTER TABLE `interest`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `origin`
+--
+ALTER TABLE `origin`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `subscribers`
+--
+ALTER TABLE `subscribers`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `subscribers`
+--
+ALTER TABLE `subscribers`
+  ADD CONSTRAINT `key` FOREIGN KEY (`origine_id`) REFERENCES `origin` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `subscriber_interest`
+--
+ALTER TABLE `subscriber_interest`
+  ADD CONSTRAINT `subscriber_interest_ibfk_1` FOREIGN KEY (`subscribers_id`) REFERENCES `subscribers` (`id`),
+  ADD CONSTRAINT `subscriber_interest_ibfk_2` FOREIGN KEY (`interest_id`) REFERENCES `interest` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
